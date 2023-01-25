37186e16f9f72fbfc5711b66dc59bf0abcfc01d9
Robinson Look <rob@lookhouse.com>
2023-01-25 13:38:36 -0600

    Challenge charts made

--- charts.js
+++ charts.js
@@ -66,29 +66,37 @@ function buildCharts(sample) {
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
     var targetSample = samplesArray.filter(sampleObj => sampleObj.id == sample);
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var targetMetadata = data.metadata.filter(mdObj => mdObj.id == sample);
 
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
     var target = targetSample[0];
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
+    var result = targetMetadata[0];
 
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var targetOTUid = target.otu_ids;
     var targetOTUlabel = target.otu_labels;
     var targetSampleValue = target.sample_values;
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
-
+    var wFreq = parseFloat(result.wfreq);
 
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
     // so the otu_ids with the most bacteria are last. 
-    var yticks = targetOUTid.slice(0,10);
-    console.log(yticks);
+    var topID = targetOTUid.slice(0,10);
+    var stringIt = topID.map(id => "OTU " + String(id));
+    var yticks = stringIt.reverse();
+    var xticks = targetSampleValue.slice(0,10);
+    console.log(targetOTUid);
+    console.log(targetSampleValue);
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
     var barData = [{
-      x: [targetSampleValue.slice(0,10)],
-      y: [yticks],
-      text: targetOTUlabel
+      x: xticks.reverse(),
+      y: yticks,
+      text: targetOTUlabel,
+      type: "bar",
+      orientation: "h"
     }
     ];
 
@@ -97,22 +105,68 @@ function buildCharts(sample) {
       title: "Top 10 Bacterial Cultures Found",
       type: "bar",
       orientation: "h"
-
     };
 
     // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
     // Deliverable 2: 1. Create the trace for the bubble chart.
-
+    var bubbleData = [{
+      x: targetOTUid,
+      y: targetSampleValue,
+      text: targetOTUlabel,
+      type: 'scatter',
+      mode: "markers",
+      marker: {
+        size: targetSampleValue,
+        color: targetOTUid,
+        colorscale: 'Earth',
+        sizemode: 'area',
+        sizeref: 0.05
+      }
+    }];
     // Deliverable 2: 2. Create the layout for the bubble chart.
-
+    var bubbleLayout = {
+      title: "Bacteria Cultures per Sample",
+      hovermode: 'closest',
+      xaxis: {
+        title: {
+          text: "OTU ID"
+        }
+      }
+    };
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
-    
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
     // Deliverable 3: 4. Create the trace for the gauge chart.
-    
+    var gaugeData = [{
+      value: wFreq,
+      type: 'indicator',
+      mode: 'gauge+number',
+      title: {text: "Scrubs per Week"},
+      gauge: {
+        axis:{
+          range: [null, 10],
+          tick0: 0,
+          dtick: 2
+        },
+        bar: {
+          color: 'black'
+        },
+        steps: [
+          {range: [0,2], color: 'red'},
+          {range: [2,4], color: 'yellow'},
+          {range: [4,6], color: 'orange'},
+          {range: [6,8], color: 'limegreen'},
+          {range: [8,10], color: 'green'},
+        ]
+      }
+    }];
     // Deliverable 3: 5. Create the layout for the gauge chart.
-
+    var gaugeLayout = {
+      title: 'Belly Button Washing Frequency'
+    };
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
-
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
